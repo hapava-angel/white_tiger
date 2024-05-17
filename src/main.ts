2274f4e036f806e58ac7986983403021cb11597b
Angelina Khapava <a.khapava.sas@gmail.com>
2024-05-17 10:59:36 +0300

    small corrects

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'http://127.0.0.1:7777',
-    credentials: true,
-  });
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
